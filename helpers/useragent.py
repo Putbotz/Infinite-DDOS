0183e3e559bb5aab518f6721411f528b64eb3102
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-27 13:16:32 +0100

    Select a random useragent from our list

--- helpers/useragent.py
+++ helpers/useragent.py
@@ -1,6 +1,12 @@
-from fake_useragent import UserAgent
+import random
 
-class useragent:
-    def random() -> str: 
-        ua: object = UserAgent()
-        return ua.random
+from typing import Union
+
+user_agent_list: list = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"]
+
+class UserAgent:
+    def random() -> Union[str, None]:
+        try:
+            return random.choice(user_agent_list)
+        except:
+            return None
