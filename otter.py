85452555552de701b7d961020e770b1ca8eae26f
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-27 19:50:25 +0100

    Update sleep time to 185

--- otter.py
+++ otter.py
@@ -17,13 +17,13 @@ class otter:
     async def run(self, result: dict) -> bool:
         try:
             await Attack.start(self, result["website"], result["method"])
-            await asyncio.sleep(183)
+            await asyncio.sleep(185)
         except:
             return False
         
-if __name__ == "__main__":    
-    otter_object: object = otter(cookie="_ga=GA1.2.975047996.1682536721; _gid=GA1.2.1846016745.1682536721; cf_clearance=MWPftL7ucNBlig445S8be.PAzHKBJSsmJLJ5ilXVU4Y-1682600087-0-160; UID=7iui5kndmqdou8cd1d6hpprdvj; _gat_gtag_UA_242087693_1=1; crisp-client%2Fsession%2F282fce01-adbf-401a-aaa1-bcd8c73ed1ec=session_a4b1837e-4d26-4f8d-9b69-77b9fd4deb1b; __RM=6e63204c05290fc27b43c6a1bd5bbf6frgh6587iui5kndmqdou8cd1d6hpprdvj6%242y%2410%24yKvDGePUQy5JqOeVOcpgfe8JoqHAWycmEnx%2F0rDIrI%2FRCLSv9h96O%2F%2Fb8b4b918cb66f10b621a5d2f7a00b4fc; X-CSRF-TOKEN=%242y%2410%24RT19TboYw.CTUHwkb8GUo.X%2FZYRU41%2FRYtbL8pmbvrO%2FdeD2BQBpK")
+if __name__ == "__main__":  
+    otter_object: object = otter(cookie="UID=9t8rg65bgvtp548hglgatq3180; _ga=GA1.2.1873893589.1682620070; _gid=GA1.2.1260745025.1682620070; crisp-client%2Fsession%2F282fce01-adbf-401a-aaa1-bcd8c73ed1ec=session_8d4129af-36d8-453f-8fe4-9a42d33a8893; cf_clearance=7rE6V3BHvgo3VtlH0zFkopfiIz7pByZJAo0rHIhsyFY-1682620490-0-160; __RM=bda744b3b23fac607bf6c30df16e841frgh6589t8rg65bgvtp548hglgatq31806%242y%2410%24spOceIL%2F70tRQcpfBsq0MuqMh%2FxyHb8V1DniVsuhxGq1nTdKnjVUC%2F%2Fca37c2f8e09d5646b199ffc9125082ab; X-CSRF-TOKEN=%242y%2410%24t3q4JrfrDGBpjTAGfUBq2.VMs3NlJdukDBfQRCC80q3FCO4bEDJZe; _gat_gtag_UA_242087693_1=1")
     result: dict = otter.get_information()
-    
+
     while True:
         asyncio.run(otter_object.run(result=result))
