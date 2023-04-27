709547b0e25a6c2860a1b1a29b6c2866b7e25117
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-27 16:14:08 +0100

    Update otter.py

--- otter.py
+++ otter.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from colorama import *
 from helpers.attack import Attack
 
@@ -12,14 +14,16 @@ class otter:
         
         return {"website": website_url, "method": method}
         
-    def send_requests(self, website: str, method: str) -> bool:
+    async def run(self, result: dict) -> bool:
         try:
-            Attack.start(self, website, method)
+            await Attack.start(self, result["website"], result["method"])
+            await asyncio.sleep(183)
         except:
             return False
         
 if __name__ == "__main__":    
-    otter_object: object = otter(cookie="_ga=GA1.2.975047996.1682536721; _gid=GA1.2.1846016745.1682536721; UID=a92boiaa3vvhg6takcvoofb40p; crisp-client%2Fsession%2F282fce01-adbf-401a-aaa1-bcd8c73ed1ec=session_6ccbc350-93c5-4f84-bfdd-e8b9d2c34f4b; __RM=47c495a4edc9f35f354852c0e29322abrgh658a92boiaa3vvhg6takcvoofb40p6%242y%2410%24wiX52bYfW6OaovUMGubZ9OWw.rxRXLyBKruaG9yfZdSwhNKvor6He%2F%2F077378cd9e095158c6f39154f2dd122f; cf_clearance=MWPftL7ucNBlig445S8be.PAzHKBJSsmJLJ5ilXVU4Y-1682600087-0-160; X-CSRF-TOKEN=%242y%2410%24Db8mSveAv2sqgm5hfA8CbuCxrhpycBZXvzx1h444sbTtwKJAXQF92; _gat_gtag_UA_242087693_1=1")
-    
+    otter_object: object = otter(cookie="_ga=GA1.2.975047996.1682536721; _gid=GA1.2.1846016745.1682536721; cf_clearance=MWPftL7ucNBlig445S8be.PAzHKBJSsmJLJ5ilXVU4Y-1682600087-0-160; UID=7iui5kndmqdou8cd1d6hpprdvj; _gat_gtag_UA_242087693_1=1; crisp-client%2Fsession%2F282fce01-adbf-401a-aaa1-bcd8c73ed1ec=session_a4b1837e-4d26-4f8d-9b69-77b9fd4deb1b; __RM=6e63204c05290fc27b43c6a1bd5bbf6frgh6587iui5kndmqdou8cd1d6hpprdvj6%242y%2410%24yKvDGePUQy5JqOeVOcpgfe8JoqHAWycmEnx%2F0rDIrI%2FRCLSv9h96O%2F%2Fb8b4b918cb66f10b621a5d2f7a00b4fc; X-CSRF-TOKEN=%242y%2410%24RT19TboYw.CTUHwkb8GUo.X%2FZYRU41%2FRYtbL8pmbvrO%2FdeD2BQBpK")
     result: dict = otter.get_information()
-    otter_object.send_requests(website=result["website"], method=result["method"])
+    
+    while True:
+        asyncio.run(otter_object.run(result=result))
