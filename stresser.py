2f0fb825a7c0a9a94f28eca69342b395906d5896
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-04-30 15:46:30 +0100

    Change brading from otter -> stresser

--- stresser.py
+++ stresser.py
@@ -3,7 +3,7 @@ import asyncio
 from colorama import *
 from helpers.attack import Attack
 
-class otter: 
+class Stresser: 
     def __init__(self, cookie: str) -> None:
         self.cookie: str = cookie
         self.base_url = "https://stresser.su"
@@ -22,8 +22,8 @@ class otter:
             return False
         
 if __name__ == "__main__":  
-    otter_object: object = otter(cookie="UID=9t8rg65bgvtp548hglgatq3180; _ga=GA1.2.1873893589.1682620070; _gid=GA1.2.1260745025.1682620070; crisp-client%2Fsession%2F282fce01-adbf-401a-aaa1-bcd8c73ed1ec=session_8d4129af-36d8-453f-8fe4-9a42d33a8893; cf_clearance=7rE6V3BHvgo3VtlH0zFkopfiIz7pByZJAo0rHIhsyFY-1682620490-0-160; __RM=bda744b3b23fac607bf6c30df16e841frgh6589t8rg65bgvtp548hglgatq31806%242y%2410%24spOceIL%2F70tRQcpfBsq0MuqMh%2FxyHb8V1DniVsuhxGq1nTdKnjVUC%2F%2Fca37c2f8e09d5646b199ffc9125082ab; X-CSRF-TOKEN=%242y%2410%24t3q4JrfrDGBpjTAGfUBq2.VMs3NlJdukDBfQRCC80q3FCO4bEDJZe; _gat_gtag_UA_242087693_1=1")
-    result: dict = otter.get_information()
+    stresser_object: object = Stresser(cookie="")
+    result: dict = Stresser.get_information()
 
     while True:
-        asyncio.run(otter_object.run(result=result))
+        asyncio.run(stresser_object.run(result=result))
